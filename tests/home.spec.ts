cbd167e4dd59e737421c676d9044cc9433cb3039
Brad <smthbh@gmail.com>
2022-12-02 22:52:31 -0500

    changed nav page to blog

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage has title and links to home page', async ({ page }) => {
+test('homepage has title and links to blog', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Rasgo - Interactive Data Catalog/);
 
   // create a locator
-  const home = page.getByRole('link', { name: 'home' });
+  const blogs = page.getByRole('link', { name: 'Blogs' })
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(home).toHaveAttribute('href', '/');
+  await expect(blogs).toHaveAttribute('href', '/blog');
 
   // Click the rasgo link.
-  await home.click();
+  await blogs.click();
 
-  // Expects the URL to be the homepage.
-  await expect(page).toHaveURL('https://www.rasgoml.com/');
+  // Expects the URL to be the blog.
+  await expect(page).toHaveURL('https://www.rasgoml.com/blog');
 });
